4ffd9d6d2f7366df848336ebad569165d37cf96a
Edward Schaefer <edwardaschaefer@gmail.com>
2018-07-25 00:27:28 -0400

    Complete Section 20: Authentication and Route Protection

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,18 +3,24 @@ import {Http} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import {map} from 'rxjs/internal/operators';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
 
-  constructor(private http: Http, private recipeService: RecipeService) { }
+  constructor(private http: Http, private recipeService: RecipeService,
+              private authService: AuthService) { }
 
   storeRecipes() {
-    return this.http.put('https://udemy-angular-5b186.firebaseio.com/recipes.json',
+    const token = this.authService.getIdToken();
+
+    return this.http.put('https://udemy-angular-5b186.firebaseio.com/recipes.json?auth=' + token,
       this.recipeService.getRecipes());
   }
   getRecipes() {
-    this.http.get('https://udemy-angular-5b186.firebaseio.com/recipes.json')
+    const token = this.authService.getIdToken();
+
+    this.http.get('https://udemy-angular-5b186.firebaseio.com/recipes.json?auth=' + token)
       .subscribe(
       (response) => {
         const recipes: Recipe[] = response.json();
